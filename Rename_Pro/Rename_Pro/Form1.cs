78dddac2ecd989c2dafc0bcf71b813f144866534
10100programer <alexmettler@hotmail.com>
2017-09-24 01:55:05 -0500

    Created a function to write a line to rich text box & got a simple getfiles button to work

--- Rename_Pro/Rename_Pro/Form1.cs
+++ Rename_Pro/Rename_Pro/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,29 @@ namespace Rename_Pro
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Only get files that begin with the letter "c."
+                string[] dirs = Directory.GetFiles(@"c:\", "*");
+                Console.WriteLine("The number of files starting with c is {0}.", dirs.Length);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                    vconsole_write(dir);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The process failed: {0}", ex.ToString());
+            }
+        }
+        void vconsole_write(string input)
+        {
+            richTextBox1.AppendText(input + "\n");
         }
     }
 }
